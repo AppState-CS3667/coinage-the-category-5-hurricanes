9a3ed500b8a83dc715df2979013e5bd16098f392
Steven <striplinsr@appstate.edu>
2021-11-03 15:31:08 -0400

    finished abstract Coin.java

--- src/Coin.java
+++ src/Coin.java
@@ -1,17 +1,19 @@
 
 public abstract Coin {
-	public String countryCode;
+	private String countryCode;
 	private int denomination;
 	private String commonName;
 	private CoinMint mint;
 	private String smelt;
-	public Coin(String cc, int dnom, String cName, CoinMint _mint) {
+	public Coin(String cc, int dnom, String cName, CoinMint _mint, String _smelt) {
 		countryCode = cc;
 		denomination = dnom;
 		commonName = cName;
 		mint = _mint;
 	}
-	public getSmelt() {
-		return smelt
-	}
+	public String getSmelt() { return smelt; }
+	public CoinMint getmint() { return mint; }
+	public String getcommonName() { return commonName; }
+	public int getDenom() { return denomination; }
+	public String getCountryCode() { return countryCode; }
 }
