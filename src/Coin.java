e60930920fd168e269fd5e8ea2bfa6d2d5213a14
striplinsr <89102812+striplinsr@users.noreply.github.com>
2021-11-03 16:33:45 -0400

    Update Coin.java

--- src/Coin.java
+++ src/Coin.java
@@ -3,17 +3,14 @@ public abstract class Coin {
 	private String countryCode;
 	private int denomination;
 	private String commonName;
-	private String mint;
 	private String smelt;
-	public Coin(String cc, int dnom, String cName, CoinMint _mint, String _smelt) {
+	public Coin(String cc, int dnom, String cName, String _smelt) {
 		countryCode = cc;
 		denomination = dnom;
 		commonName = cName;
-		mint = _mint;
 		smelt = _smelt;
 	}
 	public String getSmelt() { return smelt; }
-	public String getmint() { return mint; }
 	public String getcommonName() { return commonName; }
 	public int getDenom() { return denomination; }
 	public String getCountryCode() { return countryCode; }
