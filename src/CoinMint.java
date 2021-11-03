d9af368b1b0b2da1a5bce30af01eacb5e46ddb45
cocoacacao <89261916+longcm2@users.noreply.github.com>
2021-11-03 16:52:28 -0400

    Added factory code for making cookies

--- src/CoinMint.java
+++ src/CoinMint.java
@@ -1,3 +1,54 @@
-public abstract class CoinMint {
+import java.util.Random;
 
+public abstract class CoinMint {
+	protected boolean fail = false;
+	private Random rand = new Random();
+	
+	public void resetFail() {
+		fail = false;
+	}
+	
+	public void manufacture() {
+		System.out.println("Smelting some amount of metal...");
+	}
+	
+	public void inspect() {
+		System.out.println("Smelting some amount of metal...");
+		
+		if (rand.nextInt(12) == 4) {
+			fail = true;
+		}
+		
+		failCheck("inspecting");
+	}
+	
+	public void smoothe() {
+		if (!fail) {
+			System.out.println("Smoothing the metal... This coin be crusty...");
+			
+			if (rand.nextInt(2000) == 39) {
+				fail = true;
+			}
+	
+			failCheck("smoothing");
+		}
+	}
+	
+	public void buff() {
+		if (!fail) {
+			System.out.println("Buffing the metal. This won't remain shiny for long.");
+			
+			if (rand.nextInt(2000) == 39) {
+				fail = true;
+			}
+			
+			failCheck("buffing");
+		}
+	}
+	
+	public void failCheck(String process) {
+		if (fail) {
+			System.out.println("Oh no! The " + process + " process failed!");
+		}
+	}
 }
