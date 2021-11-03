4005fa60c7f1135cfc9ac3feb884b26d78ef9926
Steven <striplinsr@appstate.edu>
2021-11-03 10:49:49 -0400

    caleb made me do it

---
+++ src/Coin.java
@@ -0,0 +1,17 @@
+
+public abstract Coin {
+	public String countryCode;
+	private int denomination;
+	private String commonName;
+	private CoinMint mint;
+	private String smelt;
+	public Coin(String cc, int dnom, String cName, CoinMint _mint) {
+		countryCode = cc;
+		denomination = dnom;
+		commonName = cName;
+		mint = _mint;
+	}
+	public getSmelt() {
+		return smelt
+	}
+}
