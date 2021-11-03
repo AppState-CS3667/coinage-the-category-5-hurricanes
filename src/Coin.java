c864531e6a5c2b13bf7a8a3b4f9ca70f3735e6a8
longcm2 <89261916+longcm2@users.noreply.github.com>
2021-11-03 16:09:23 -0400

    Update Coin.java

--- src/Coin.java
+++ src/Coin.java
@@ -3,7 +3,7 @@ public abstract class Coin {
 	private String countryCode;
 	private int denomination;
 	private String commonName;
-	private CoinMint mint;
+	private String mint;
 	private String smelt;
 	public Coin(String cc, int dnom, String cName, CoinMint _mint, String _smelt) {
 		countryCode = cc;
@@ -13,7 +13,7 @@ public abstract class Coin {
 		smelt = _smelt;
 	}
 	public String getSmelt() { return smelt; }
-	public CoinMint getmint() { return mint; }
+	public String getmint() { return mint; }
 	public String getcommonName() { return commonName; }
 	public int getDenom() { return denomination; }
 	public String getCountryCode() { return countryCode; }
