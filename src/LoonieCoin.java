0a02134346af93ce9b81768e8f61ee53bc6f9865
Steven <striplinsr@appstate.edu>
2021-11-03 16:52:16 -0400

    finsihed loonie coin and now need to change all altehr produced coins

---
+++ src/LoonieCoin.java
@@ -0,0 +1,11 @@
+
+public LooineCoin extends coin {
+	public LoonieCoin(bool _fail) {
+		bool fail = _fail;
+		double deno = 1.0;
+		String commonName = "Loonie"
+		String cc = "CAD";
+		String smelt = "Unknown";
+		super(cc, deno, commonName, smelt, fail);
+	}
+}
