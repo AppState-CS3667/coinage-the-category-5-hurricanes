cf13c0d8dc2eecd4f946a7ae2436c3e6b19fe9c2
Phillip Davis <phdavis1027@gmail.com>
2021-11-03 22:01:24 -0400

    starting to build out coinmint test

--- test/CoinMintTest.java
+++ test/CoinMintTest.java
@@ -1,3 +1,28 @@
 public class CoinMintTest{
 
+    private static CoinMint testMint;
+    private static String expectedOutput;
+    private static String actualOutput;
+
+    @BeforeEach
+    public static void setUp(){
+        testMint = CointMint.new DummyCoinMint();
+
+    }
+
+    @AfterEach
+    public static void cleanUp(){
+        checkResult();
+    } 
+
+    public static void checkResult(){
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public static testManufacture(){
+        expectedOutput = "Smelting some amount of metal...";
+        actualOutput = testMint.manufacture();
+    }
+
 }
