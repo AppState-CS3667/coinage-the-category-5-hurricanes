2f107a5b95bae15e26ec986318f5e81d368a1d4b
cocoacacao <89261916+longcm2@users.noreply.github.com>
2021-11-05 10:35:19 -0400

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -54,14 +54,15 @@ public class Demo {
 		coin = cadmint.makeCoin(0.05);
 		System.out.println(coin);
 		
-		System.out.println("Making and printing quarters until one fails...");
+		System.out.println("\n\nMaking and printing quarters until one fails...\n");
 		
 		Coin failCoin = usdmint.makeCoin(0.25);
 		
 		while (!failCoin.getFail()) {
 			failCoin = usdmint.makeCoin(0.25);
+			System.out.println("\n" + failCoin + "\n");
 		}
 		
-		System.out.println(failCoin + "\n");
+		
 	}
 }
